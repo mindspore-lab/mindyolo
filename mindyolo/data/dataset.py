665d494a13066f915ade74754a1d32f7494e82d5
yuedongli1 <2660417142@qq.com>
2023-02-01 20:04:54 +0800

    fix cocodataset

--- mindyolo/data/dataset.py
+++ mindyolo/data/dataset.py
@@ -1,17 +1,9 @@
 import os
-import numpy as np
-import copy
 import cv2
-import yaml
+import copy
+import numpy as np
 
-import mindspore.dataset as de
-
-from . import transforms
-
-from general import show_img_with_label
-
-# from utils.logger import setup_logger
-# logger = setup_logger(__name__)
+import transforms
 
 
 class COCODataset:
@@ -21,7 +13,6 @@ class COCODataset:
         dataset_dir (str): root directory for dataset.
         image_dir (str): directory for images.
         anno_path (str): annotation file path.
-        data_fields (list): key name of data dictionary, at least have 'image'.
         sample_num (int): number of samples to load, -1 means all.
         load_crowd (bool): whether to load crowded ground-truth.
             False as default
@@ -29,74 +20,72 @@ class COCODataset:
         empty_ratio (float): the ratio of empty record number to total
             record's, if empty_ratio is out of [0. ,1.), do not sample the
             records and use all the empty entries. 1. as default
-        repeat (int): repeat times for dataset, use in benchmark.
     """
 
     def __init__(self,
                  dataset_dir=None,
                  image_dir=None,
                  anno_path=None,
-                 img_size=None,
-                 data_fields=['image'],
                  sample_num=-1,
-                 repeat=1,
                  load_crowd=False,
                  allow_empty=False,
-                 norm_bbox=True,
                  empty_ratio=1.,
-                 muliti_imgs_transforms=None):
+                 muliti_imgs_transforms=None,
+                 most_boxes_per_img=160):
         self.dataset_dir = dataset_dir if dataset_dir is not None else ''
         self.anno_path = anno_path
         self.image_dir = image_dir if image_dir is not None else ''
-        self.img_size = img_size
         self.sample_num = sample_num
-        self.repeat = repeat
-        self._epoch = 0
         self.load_image_only = False
         self.load_crowd = load_crowd
         self.allow_empty = allow_empty
-        self.norm_bbox = norm_bbox
         self.empty_ratio = empty_ratio
         self.muliti_imgs_transforms = muliti_imgs_transforms
-        self.data_fields = data_fields
+        self.most_boxes_per_img = most_boxes_per_img
         self.parse_dataset()
 
     def __len__(self, ):
-        return len(self.imgs_records) * self.repeat
+        return len(self.imgs_records)
 
     def __call__(self, *args, **kwargs):
         return self
 
     def __getitem__(self, idx):
         n = len(self.imgs_records)
-        if self.repeat > 1:
-            idx %= n
-        # data batch
-        records_out = copy.deepcopy(self.imgs_records[idx])
-        gt_bbox, gt_class = records_out['gt_box'], records_out['gt_class']
-        img = cv2.imread(records_out['im_file'])  # BGR
-        img_size = self.img_size
-        for t in self.muliti_imgs_transforms:
-            for k, v in t.items():
-                op_cls = getattr(transforms, k)
-                f = op_cls(**v)
-                if k == 'mosaic':
+        record_out = copy.deepcopy(self.imgs_records[idx])
 
-
-
-        # img = img.transpose(2, 0, 1)  # BGR to RGB
-        # img = np.ascontiguousarray(img)
-
-        return img, records_out
+        # multi_imgs_transforms
+        if self.muliti_imgs_transforms:
+            for t in self.muliti_imgs_transforms:
+                for k, v in t.items():
+                    op_cls = getattr(transforms, k)
+                    f = op_cls(**v)
+                    if k == 'Mosaic':
+                        records_outs = [record_out,] + [copy.deepcopy(self.imgs_records[np.random.randint(n)]) for _ in range(3)] # 3 additional image
+                    for record_out in records_outs:
+                        if 'image' not in record_out:
+                            img_path = record_out['im_file']
+                            record_out['image'] = cv2.imread(img_path)  # BGR
+                    record_out = f(records_outs)
+        else:
+            img_path = record_out['im_file']
+            record_out['image'] = cv2.imread(img_path)  # BGR
+        real_gt_bbox = record_out['gt_bbox']
+        real_gt_class = record_out['gt_class']
+        nL = real_gt_bbox.shape[0]
+        gt_bbox = np.full((self.most_boxes_per_img, 4), -1, dtype=np.float32)
+        gt_bbox[:min(nL, self.most_boxes_per_img), :] = real_gt_bbox[:min(nL, self.most_boxes_per_img), :]
+        gt_class = np.full((self.most_boxes_per_img, 1), -1, dtype=np.int32)
+        gt_class[:min(nL, self.most_boxes_per_img), :] = real_gt_class[:min(nL, self.most_boxes_per_img), :]
+        return record_out['image'], record_out['h'], record_out['w'], gt_class, gt_bbox
 
     def _sample_empty(self, records, num):
         # if empty_ratio is out of [0. ,1.), do not sample the records
         if self.empty_ratio < 0. or self.empty_ratio >= 1.:
             return records
-        import random
         sample_num = min(
             int(num * self.empty_ratio / (1 - self.empty_ratio)), len(records))
-        records = random.sample(records, sample_num)
+        records = np.random.sample(records, sample_num)
         return records
 
     def parse_dataset(self):
@@ -130,9 +119,7 @@ class COCODataset:
             im_fname = img_anno['file_name']
             im_w = float(img_anno['width'])
             im_h = float(img_anno['height'])
-
-            im_path = os.path.join(image_dir,
-                                   im_fname) if image_dir else im_fname
+            im_path = os.path.join(image_dir, im_fname) if image_dir else im_fname
             is_empty = False
             if not os.path.exists(im_path):
                 # logger.warning('Illegal image file: {}, and it will be '
@@ -150,7 +137,7 @@ class COCODataset:
                 'im_id': np.array([img_id]),
                 'h': im_h,
                 'w': im_w,
-            } if 'image' in self.data_fields else {}
+            }
 
             if not self.load_image_only:
                 ins_anno_ids = coco.getAnnIds(
@@ -168,23 +155,16 @@ class COCODataset:
                         if not any(np.array(inst['bbox'])):
                             continue
 
-                    x1, y1, box_w, box_h = inst['bbox']
-                    x2 = x1 + box_w
-                    y2 = y1 + box_h
+                    x, y, box_w, box_h = inst['bbox']
                     eps = 1e-5
-                    if inst['area'] > 0 and x2 - x1 > eps and y2 - y1 > eps:
-                        if not self.norm_bbox:
-                            inst['clean_bbox'] = [
-                                round(float(x), 3) for x in [x1, y1, x2, y2]
-                            ]
-                        else:
-                            norm_clean_bbox = [
-                                x1 * 1.0 / im_w, y1 * 1.0 / im_h,
-                                x2 * 1.0 / im_w, y2 * 1.0 / im_h
-                            ]
-                            inst['clean_bbox'] = [
-                                round(float(x), 6) for x in [norm_clean_bbox]
-                            ]
+                    if inst['area'] > 0 and box_w > eps and box_h > eps:
+                        norm_clean_bbox = [
+                            (x + box_w/2) * 1.0 / im_w, (y + box_h/2) * 1.0 / im_h,
+                            box_w * 1.0 / im_w, box_h * 1.0 / im_h
+                        ]
+                        inst['clean_bbox'] = [
+                            round(float(x), 6) for x in norm_clean_bbox
+                        ]
                         bboxes.append(inst)
                     # else:
                     #     logger.warning(
@@ -211,22 +191,25 @@ class COCODataset:
                     is_crowd[i][0] = box['iscrowd']
 
                     if 'segmentation' in box and box['iscrowd'] == 1:
-                        gt_poly[i] = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+                        gt_poly[i] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
                     elif 'segmentation' in box and box['segmentation']:
-                        if not np.array(box['segmentation']
-                                        ).size > 0 and not self.allow_empty:
+                        if not np.array(box['segmentation']).size > 0 and not self.allow_empty:
                             bboxes.pop(i)
                             gt_poly.pop(i)
                             np.delete(is_crowd, i)
                             np.delete(gt_class, i)
                             np.delete(gt_bbox, i)
                         else:
-                            gt_poly[i] = box['segmentation']
+                            gt_poly[i] = box['segmentation'][0]
                         has_segmentation = True
 
-                if has_segmentation and not any(
-                        gt_poly) and not self.allow_empty:
+                if has_segmentation and not any(gt_poly) and not self.allow_empty:
                     continue
+
+                gt_poly = [np.array(x, dtype=np.float32).reshape(-1, 2) for x in gt_poly]
+                for x in gt_poly:
+                    x[:, 0] /= im_w
+                    x[:, 1] /= im_h
 
                 gt_rec = {
                     'is_crowd': is_crowd,
@@ -236,8 +219,7 @@ class COCODataset:
                 }
 
                 for k, v in gt_rec.items():
-                    if k in self.data_fields:
-                        img_rec[k] = v
+                    img_rec[k] = v
 
             # logger.debug('Load file: {}, im_id: {}, h: {}, w: {}.'.format(
             #     im_path, img_id, im_h, im_w))
@@ -254,28 +236,3 @@ class COCODataset:
             empty_records = self._sample_empty(empty_records, len(records))
             records += empty_records
         self.imgs_records = records
-
-
-def create_dataloader(img_size, batch_size, epoch_size=300, hyp=None, augment=False,
-                      cache=False, pad=0.0, rect=False, rank=0, rank_size=1, num_parallel_workers=8,
-                      shuffle=True, drop_remainder=True, image_weights=False, quad=False, max_box_per_img=160, prefix=''):
-    # Make sure only the first process in DDP process the dataset first, and the following others can use the cache
-    dataset = COCODataset(dataset_dir='D:\datasets\coco2017', image_dir='./train2017', anno_path='./annotations/instances_train2017.json', muliti_imgs_transforms=hyp['muliti_imgs_transforms'])
-    ds = de.GeneratorDataset(dataset)
-    ds = ds.batch(batch_size)
-    ds = ds.repeat(epoch_size)
-
-    return ds
-
-
-if __name__ == '__main__':
-    hyp_path = 'D:\mymindyolo\configs\hyp\hyp.scratch.yaml'
-    with open(hyp_path) as f:
-        hyp = yaml.load(f, Loader=yaml.SafeLoader)  # load hyps
-
-    ds = create_dataloader(img_size=[640, 640], batch_size=8, hyp=hyp)
-    data_loader = ds.create_dict_iterator(output_numpy=True, num_epochs=1)
-    print('done')
-    for i, data in enumerate(data_loader):
-        imgs, label_outs, img_files = data["img"], data["label_out"], data["img_files"]
-    print('donedone')
