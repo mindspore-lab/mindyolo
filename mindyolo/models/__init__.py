ae6d7bb8d3e1abb46ea0b109b288ab8aebb84579
unknown <2660417142@qq.com>
2024-10-10 18:51:18 +0800

    add yolov9

--- mindyolo/models/__init__.py
+++ mindyolo/models/__init__.py
@@ -1,10 +1,11 @@
 from . import (heads, initializer, layers, losses, model_factory, yolov3,
-               yolov4, yolov5, yolov7, yolov8)
+               yolov4, yolov5, yolov7, yolov8, yolov9)
 
 __all__ = []
 __all__.extend(heads.__all__)
 __all__.extend(layers.__all__)
 __all__.extend(losses.__all__)
+__all__.extend(yolov9.__all__)
 __all__.extend(yolov8.__all__)
 __all__.extend(yolov7.__all__)
 __all__.extend(yolov5.__all__)
@@ -25,4 +26,5 @@ from .yolov4 import *
 from .yolov5 import *
 from .yolov7 import *
 from .yolov8 import *
+from .yolov9 import *
 from .yolox import *
